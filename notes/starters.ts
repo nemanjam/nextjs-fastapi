67f1bde5938600facfdbb7525e748448a51a783a
nemanjam <nemanja.mitic.elfak@hotmail.com>
2023-10-25 13:47:22 +0200

    add notes

---
+++ notes/starters.ts
@@ -0,0 +1,37 @@
+
+// Starter projects
+
+// map next.js to fastapi, redirect `next.config.js`, small - hello world
+https://github.com/digitros/nextjs-fastapi
+------
+// simple, minimal, sqlmodel orm
+https://github.com/testdrivenio/fastapi-sqlmodel-alembic
+-------
+// fastapi backend only
+//  excellent fastapi, rocketry as scheduler, rabbitmq
+https://github.com/kamranabdicse/fastapi-postgres-boilerplate
+// another, better, redis broker, terraform
+https://github.com/jonra1993/fastapi-alembic-sqlmodel-async
+// kazakhstan, maybe this, SAMO redis, nema rabbitmq i celery
+https://github.com/zhanymkanov/fastapi_production_template
+------
+// losi
+// solidan, mali, fastapi, hackernews kandidat
+https://github.com/innovatorved/whisper.api
+// docs, los outdated
+// https://github.com/tiangolo/full-stack-fastapi-postgresql
+-----
+// dobar, linkedin, jedini ima rabbitmq broker
+https://github.com/AdrianPayne/fastapi-rocket-boilerplate
+// promenio github username, sad je:
+https://github.com/asacristani/fastapi-rocket-boilerplate
+------
+// generator, redis, rabbitmq, logging
+https://github.com/s3rius/FastAPI-template
+-----
+// two similar with arq
+// another from linkedin
+https://github.com/igormagalhaesr/FastAPI-boilerplate
+// dobar zapravo, kubernetes
+https://github.com/Kludex/fastapi-microservices
+
